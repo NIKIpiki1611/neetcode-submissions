b84e62b0a736e17ac860270cfe8eb07b225e1371
Nikunj Maheshwari 1209 <nm0542629@gmail.com>
2026-09-05 17:29:56 +0530

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.py
@@ -0,0 +1,13 @@
+def lengthOfLongestSubstring(s):
+    last_seen = {}
+    left = 0
+    longest = 0
+
+    for right, char in enumerate(s):
+        if char in last_seen and last_seen[char] >= left:
+            left = last_seen[char] + 1
+
+        last_seen[char] = right
+        longest = max(longest, right - left + 1)
+
+    return longest
